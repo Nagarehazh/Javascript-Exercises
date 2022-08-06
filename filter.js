4f265771ebfd3932ba896ef844733f2b1f6253b9
Nagarehazh <proyectosexitososya@gmail.com>
2022-08-06 06:33:17 -0500

    It filters your arrays based on a specific test
    Similar to the forEach() method, the filter() method also accepts a function and that function performs some work on each of the items in the array.

---
+++ filter.js
@@ -0,0 +1,7 @@
+//Another very useful method on the array is the filter() method. It filters your arrays based on a specific test. Those array items that pass the test are returned.
+const nums = [0,10,20,30,40,50];
+nums.filter( function(num) {
+    return num > 20;
+})
+
+//Result:  [30,40,50]
