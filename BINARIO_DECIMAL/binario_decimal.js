6a373dd98133621638d089a4c87697c5c1788c0e
Nagarehazh <proyectosexitososya@gmail.com>
2022-08-16 20:37:37 -0500

    short Binary Decimal, Decimal Binary

--- BINARIO_DECIMAL/binario_decimal.js
+++ BINARIO_DECIMAL/binario_decimal.js
@@ -1,19 +1,21 @@
 function binarioDecimal(num){
-    let decimal = 0;
-    for(let i in num){
-    decimal += num[i] * Math.pow(2, (num.length - 1 - i));
-    }
-    return decimal; 
+    // let decimal = 0;
+    // for(let i in num){
+    // decimal += num[i] * Math.pow(2, (num.length - 1 - i));
+    // }
+    // return decimal; 
+    return parseInt(num, 2);
 }
 
 function decimalBinario(num){
-    let binario = [];
-    while(num !== 0 ){
-        let residuo = num%2;
-        binario.push(residuo);
-        num = Math.floor(num/2);
-    }
-    return binario.reverse().toString().replace(/,/g, "");
+    // let binario = [];
+    // while(num !== 0 ){
+    //     let residuo = num%2;
+    //     binario.push(residuo);
+    //     num = Math.floor(num/2);
+    // }
+    // return binario.reverse().toString().replace(/,/g, "");
+    return num.toString(2);
 }
 
 // binarioDecimal("100101");
